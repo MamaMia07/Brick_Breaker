8d70b7062272ffc29ee2987c0686585a83ca79db
MamaMia07 <kokoszkam@gazeta.pl>
2023-07-01 00:14:07 +0200

    game_state class modification

--- brick_breaker_game.py
+++ brick_breaker_game.py
@@ -16,6 +16,42 @@ class GameStage():
         text_surface = game_font.render(text_score, False,(0, 255,0))
         win.blit(text_surface, pos)
 
+    def extra_balls_display(self):
+        rand_extra = random.randrange(500)
+        if rand_extra == 1:
+            ball_container = gbe.BallContainer(img_path)
+            ball_container_group.add(ball_container)
+        level_range = 1000 if game.level == "level_1" else 700
+        rand_fast = random.randrange(level_range)
+        if rand_fast == 1:
+            fast_ball = gbe.FastBall(img_path)
+            fast_ball_group.add(fast_ball)
+
+
+    def ball_update(self):
+        for sprite in ball_group:
+            sprite.collision(ball_group, brick_group, bar_group)
+            game.score += sprite.collid_points
+            sprite.collid_points = 0
+            sprite.movement(ball_group)
+            if sprite.fall == True:
+                game.counter -= 1
+                sprite.fall = False
+
+    def extra_balls_update(self):
+        for sprite in ball_container_group:
+            sprite.movement(ball_container_group)
+            sprite.catch(ball_container_group, bar_group, extra_balls_group)
+        for sprite in fast_ball_group:
+            sprite.movement(fast_ball_group)
+            sprite.catch(fast_ball_group, bar_group, ball_group)
+        for sprite in extra_balls_group:
+            sprite.collision(extra_balls_group, brick_group, bar_group)
+            game.score += sprite.collid_points
+            sprite.collid_points = 0
+            sprite.movement(extra_balls_group)
+    
+
     def event_response(self, event):
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -44,7 +80,6 @@ class GameStage():
 
     def counting(self):
         pygame.mouse.set_visible(False) 
-
         ball = gbe.Ball(img_path)
         ball_group.add(ball)
         extra_balls_group.empty()
@@ -52,7 +87,6 @@ class GameStage():
         ball_container_group.empty()
         brick_group.empty()
         bar.rect.x , bar.rect.y = 260, 430
-
         count_lev = {1:["count_lev1_3.png", "count_lev1_2.png","count_lev1_1.png"],
                      2:["count_lev2_3.png", "count_lev2_2.png","count_lev2_1.png"],
                      3:["count_lev3_3.png","count_lev3_2.png", "count_lev3_1.png"]}
@@ -86,62 +120,29 @@ class GameStage():
             if len(brick_group)== 0:
                 pygame.time.wait(1000)
                 if game.level == "level_3":
-                    #self.level_nb = 1
                     game.level = "new_game"
                 else:
                     self.level_nb += 1
                     game.counter = 3
                     game.level = "counting"
-
         if game.counter == 0:
             pygame.time.wait(1000)
             game.level = "new_game"
-            #self.level_nb = 1
 
         win.blit(background,(0,0))
-
-        rand_extra = random.randrange(500)
-        if rand_extra == 1:
-            ball_container = gbe.BallContainer(img_path)
-            ball_container_group.add(ball_container)
-    
-        if game.level == "level_1": level_range = 1000
-        else: level_range = 400
-        rand_fast = random.randrange(level_range)
-        if rand_fast == 1:
-            fast_ball = gbe.FastBall(img_path)
-            fast_ball_group.add(fast_ball)
+        self.extra_balls_display()
 
         bar_group.draw(win)
         bar_group.update()
         bar.bar_image(game.counter)
 
         ball_group.draw(win)
-        for sprite in ball_group:
-            sprite.collision(ball_group, brick_group, bar_group)
-            game.score += sprite.collid_points
-            sprite.collid_points = 0
-            sprite.movement(ball_group)
-            if sprite.fall == True:
-                game.counter -= 1
-                sprite.fall = False
-
+        self.ball_update()
         extra_balls_group.draw(win)
-        for sprite in extra_balls_group:
-            sprite.collision(extra_balls_group, brick_group, bar_group)
-            game.score += sprite.collid_points
-            sprite.collid_points = 0
-            sprite.movement(extra_balls_group)
 
         ball_container_group.draw(win)
-        for sprite in ball_container_group:
-            sprite.movement(ball_container_group)
-            sprite.catch(ball_container_group, bar_group, extra_balls_group)
-
         fast_ball_group.draw(win)
-        for sprite in fast_ball_group:
-            sprite.catch(fast_ball_group, bar_group, ball_group)
-            sprite.movement(fast_ball_group)
+        self.extra_balls_update()
 
         brick_group.draw(win)
         brick_group.update() 
@@ -159,7 +160,6 @@ class GameStage():
                 game.level = "counting"
                 game.counter = 3
                 game.score = 0
-                #self.level_nb = 1 
             if exit_btn.rect.collidepoint(pygame.mouse.get_pos()) and event.type == pygame.MOUSEBUTTONDOWN:
                     pygame.quit()
                     sys.exit()
